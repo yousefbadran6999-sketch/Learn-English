5a9f255b916eac1aa45ac3dbf9acdf2a245d0048
yousefbadran6999-sketch <yousefbadran6999@gmail.com>
2026-04-07 00:14:46 +0200

    Create lessons.js

---
+++ src/data/lessons.js
@@ -0,0 +1,71 @@
+export const lessons = [
+  {
+    id: 1,
+    title: '👋 التحيات',
+    emoji: '👋',
+    words: [
+      {
+        word: 'Hello',
+        translation: 'مرحباً',
+        image: 'https://cdn-icons-png.flaticon.com/512/1946/1946429.png',
+        audioPhrase: 'Hello. Nice to meet you.'
+      },
+      {
+        word: 'Good morning',
+        translation: 'صباح الخير',
+        image: 'https://cdn-icons-png.flaticon.com/512/3039/3039435.png',
+        audioPhrase: 'Good morning, how are you today?'
+      },
+      {
+        word: 'Good night',
+        translation: 'تصبح على خير',
+        image: 'https://cdn-icons-png.flaticon.com/512/2972/2972631.png',
+        audioPhrase: 'Good night, sleep well.'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: '🔢 الأرقام',
+    emoji: '🔢',
+    words: [
+      {
+        word: 'One',
+        translation: 'واحد',
+        image: 'https://cdn-icons-png.flaticon.com/512/186/186239.png',
+        audioPhrase: 'One cup of coffee, please.'
+      },
+      {
+        word: 'Two',
+        translation: 'اثنان',
+        image: 'https://cdn-icons-png.flaticon.com/512/186/186241.png',
+        audioPhrase: 'Two tickets to New York.'
+      },
+      {
+        word: 'Three',
+        translation: 'ثلاثة',
+        image: 'https://cdn-icons-png.flaticon.com/512/186/186242.png',
+        audioPhrase: 'Three apples from the market.'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: '🎨 الألوان',
+    emoji: '🎨',
+    words: [
+      {
+        word: 'Red',
+        translation: 'أحمر',
+        image: 'https://cdn-icons-png.flaticon.com/512/1183/1183672.png',
+        audioPhrase: 'The red car is fast.'
+      },
+      {
+        word: 'Blue',
+        translation: 'أزرق',
+        image: 'https://cdn-icons-png.flaticon.com/512/1183/1183697.png',
+        audioPhrase: 'The sky is blue today.'
+      }
+    ]
+  }
+];
